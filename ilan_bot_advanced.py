92843136a05fadd42210ff99a0c8d6c63b80920d
TradersEntertainment <163758188+TradersEntertainment@users.noreply.github.com>
2025-04-22 00:04:18 +0300

    Update ilan_bot_advanced.py

--- ilan_bot_advanced.py
+++ ilan_bot_advanced.py
@@ -5,7 +5,7 @@ import time
 import os
 
 # Telegram bilgileri
-TOKEN = "7797092615:AAEdkKXEuMnzbysSogVueCHjx7heWlKf3bk"
+TOKEN = os.environ.get("BOT_TOKEN")
 CHAT_ID = 515442086  # Sadece senin chat ID'n
 bot = telebot.TeleBot(TOKEN)
 
